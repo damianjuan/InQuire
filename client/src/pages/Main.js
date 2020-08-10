630d322a47f8fd2087ed61b0e4387d02d264011e
Sang Bae <sw2.bae@gmail.com>
2020-08-10 17:29:18 -0500

    signup/login backend finished

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import API from "../utils/API";
 
 function Main() {
+
+    function logInHandler(event) {
+        event.preventDefault();
+        API.logIn({
+            email: event.target.email.value,
+            password: event.target.password.value
+        })
+            .then(() => {
+                window.location.replace("/create-survey");
+            })
+    }
+
     return (
         <main className="bg-gray-300 m-8">
             <h1 className="pt-8 text-2xl text-center">Welcome</h1>
-            <div className="flex flex-col mx-auto p-4 w-full text-xl items-start">
-                <input className="my-2 w-3/6" type="text" name="prompt" placeholder="Email" />
-                <input className="my-2 w-3/6" type="text" name="prompt" placeholder="Password" />
-            </div>
+            <form onSubmit={logInHandler}>
+                <input className="my-2 w-3/6" type="text" name="email" placeholder="Email" />
+                <br></br>
+                <input className="my-2 w-3/6" type="password" name="password" placeholder="Password" />
+                <br></br>
+                <button type="submit">Log In</button>
+            </form>
             <div className="flex flex-col mx-auto p-4 w-full text-lg">
-                <Link className="flex-1" to={process.env.PUBLIC_URL + '/create-survey'}>  Login </Link><br />
                 <Link className="flex-1" to={process.env.PUBLIC_URL + '/create-survey'}>Login as Guest</Link><br />
                 <Link className="flex-1" to={process.env.PUBLIC_URL + '/signup'}>Sign Up</Link>
             </div>
         </main>
     );
 }
+
 
 export default Main;
