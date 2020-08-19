0891c0267ec457d191faa3e302ef72383f6eddbb
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-08-18 22:44:34 -0500

    express session set up complete and check for user logged in complete. still need to add protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./pages/Main";
@@ -6,20 +6,57 @@ import SignUp from "./pages/SignUp"
 import CreateSurvey from "./pages/CreateSurvey";
 import HomePage from "./pages/HomePage";
 import ViewResults from './pages/ViewResults';
+import axios from "axios";
 
 function App() {
+
+
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    axios.get('api/checkAuthentication').then(res => {
+      setUser(res.data.user);
+    })
+  }, []);
+
+  function loggedInRoutes() {
+    if (user && user.email) {
+      return (
+        <>
+          <Route path="/home/" component={HomePage} />
+          <Route path="/create-survey/" component={CreateSurvey} />
+          <Route path="/results/:id" component={ViewResults} />
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Route exact path="/signup/" component={SignUp} />
+          <Route component={Main} />
+        </>
+      )
+    }
+
+  };
+
+
   return (
     <Router>
       <Header />
       <Switch>
-        <Route exact path="/signup/" component={SignUp} />
-        <Route exact path="/home/" component={HomePage} />
-        <Route exact path="/create-survey/" component={CreateSurvey} />
-        <Route exact path="/results/:id" component={ViewResults} />
-        <Route path="*" component={Main} />
+        {
+          user ? (
+            <Route path="/home/" component={HomePage} />
+            <Route path="/create-survey/" component={CreateSurvey} />
+            <Route path="/results/:id" component={ViewResults} />
+         ) : (
+              <Route exact path="/signup/" component={SignUp} />
+              <Route component={Main} />
+         )
+        }
       </Switch>
-    </Router>
-  );
-}
+    </Router>)
+};
+
 
 export default App;
