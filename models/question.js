dc16f71d8abaa46cbd0d90d926ef8a25cf6fb018
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-08-20 12:16:31 -0500

    insert free responses

--- models/question.js
+++ models/question.js
@@ -3,7 +3,6 @@ module.exports = (sequelize, DataTypes) => {
   const Question = sequelize.define(
     'Question',
     {
-      // survey_id: DataTypes.INTERGER,
       question_title: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -17,16 +16,10 @@ module.exports = (sequelize, DataTypes) => {
         validation: {
           len: [1]
         }
-      }//,
-    //   choices: {
-    //     type: DataTypes.STRING,
-    //     allowNull: true,
-    //     validation: {
-    //       len: [1]
-    //     }
-    //   }
+      }
     }
   );
+
   Question.associate = function (models) {
     // associations can be defined here
     Question.belongsTo(models.Survey, {
@@ -35,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false
       }
     });
+    
     Question.hasMany(models.Answer, {
       onDelete: "cascade",
       foreignKey: {
