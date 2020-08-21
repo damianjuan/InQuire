793d258d8e1b1dc880d57c455594b97333b72f85
Sang Bae <sw2.bae@gmail.com>
2020-08-20 23:33:45 -0500

    takeSurvey Page Done

--- client/src/pages/TakeSurvey.js
+++ client/src/pages/TakeSurvey.js
@@ -4,7 +4,7 @@ import API from "../utils/API";
 
 function TakeSurvey() {
     const [question, setQuestion] = useState();
-    const [answer, setanswer] = useState([]);
+    const [answer, setAnswer] = useState([]);
     const { id } = useParams();
     // console.log(id);
 
@@ -14,9 +14,18 @@ function TakeSurvey() {
             .catch(err => console.log(err));
     }, []);
 
-    function submitBtn(event) {
-        event.preventDefault();
-        console.log(event.target.RadioGroup);
+    function submitBtn() {
+        API.submitResults(answer);
+        // .then(window.localStorage.replace("/thankyou"));
+    }
+
+    function handleOnChange(e) {
+        const userAnswer =
+            [...document.querySelectorAll('input')]
+                .filter(x => x.checked)
+                .map(x => parseInt(x.value));
+        // console.log(userAnswer);
+        setAnswer(userAnswer);
     }
 
     function renderQuestions() {
@@ -26,57 +35,58 @@ function TakeSurvey() {
                 </>
             );
         } else {
-            console.log(question);
+            // console.log(question);
             return (
-                <div>
-                    <main className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg text-center">
+                <main>
+                    <h2 className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg text-center">
                         Survey Title : {question.survey_name}
-                    </main>
+                    </h2>
                     {question.Questions.map((item, index) => {
-                        if (item.question_type == "multipleChoice") {
+                        if (item.question_type === "multipleChoice") {
                             return (
-                                <main className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg">
+                                <article className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg" key={index}>
                                     {index + 1}) {item.question_title}<hr />
-                                    {item.Answers.map((choice, index) => {
-                                        return (
-
-                                            <ul>
-                                                <label key={index}>
-                                                    <input className="m-4" name="RadioGroup" value={choice.id} type="radio" />
-                                                    {choice.answer}
-                                                </label>
-                                            </ul>
-
-                                        )
-                                    })}
-                                </main>
+                                    <ul>
+                                        {item.Answers.map((choice, index) => {
+                                            return (
+                                                <li onChange={handleOnChange} key={index}>
+                                                    <label>
+                                                        <input className="m-4" name={item.id} value={choice.id} type="radio" />
+                                                        {choice.answer}
+                                                    </label>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </article>
                             )
-                        } else if (item.question_type == "selectApply") {
+                        } else if (item.question_type === "selectApply") {
                             return (
-                                <main className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg">
+                                <article className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg" key={index}>
                                     {index + 1}) {item.question_title}<hr />
-                                    {item.Answers.map((choice, index) => {
-                                        return (
+                                    <ul>
+                                        {item.Answers.map((choice, index) => {
+                                            return (
 
-                                            <ul>
-                                                <label key={index}>
-                                                    <input className="m-4" name="CheckGroup" value={choice.id} type="checkbox" />
-                                                    {choice.answer}
-                                                </label>
-                                            </ul>
-
-                                        )
-                                    })}
-                                </main>
+                                                <li onChange={handleOnChange} key={index}>
+                                                    <label>
+                                                        <input className="m-4" name={item.id} value={choice.id} type="checkbox" />
+                                                        {choice.answer}
+                                                    </label>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </article>
                             )
                         } else {
                             return (
-                                <main className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg">
+                                <article className="mx-auto my-4 p-4 w-5/6 bg-gray-300 rounded-lg" key={index}>
                                     {index + 1}) {item.question_title}<hr />
                                     <form>
                                         <input className="m-8" type="text" name="response" />
                                     </form>
-                                </main>
+                                </article>
                             )
                         }
                     })
@@ -84,7 +94,7 @@ function TakeSurvey() {
                     <div className="flex">
                         <button className="mx-auto p-2 bg-yellow-500 rounded-full w-40" type="submit" onClick={submitBtn}>Submit Survey</button>
                     </div>
-                </div >
+                </main>
             )
         }
     }
